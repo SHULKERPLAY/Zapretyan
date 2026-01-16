2d263654ac6e671a8e14c3bab2d3a17dc7a2845a
SHULKERPLAY <aleksvol2005@gmail.com>
2026-01-16 06:42:45 +0300

    Рефактор бота
    - Полный переход на использование let и const
    - Внедрение асинхронных функций и ответ дискорду о принятой команде в обработку перед ответом
    - Унификация отправки сообщений в отдельные функции
    - Вынос всех обработок в отдельные функции
    - Упрощение логики /bancheck
    - Переход на использование MessageFlags
    - Уточнение про IPv4 адреса в описании команд

--- sender/index.js
+++ sender/index.js
@@ -1,4 +1,4 @@
-const corever = 'v1.4.31';
+const corever = 'v1.4.40';
 const forbiddenChars = /['",:;<>?!@#$%^&*(){}|\[\]\/\\]/;
 //Statistics
 const { loadStats, incrementStat, statsAutoSave } = require('./botstats.js');
@@ -7,10 +7,11 @@ loadStats();
 statsAutoSave(60);
 
 const fs = require('fs');
+const fspromises = require('fs').promises;
 const path = require('path');
 const { exec } = require("child_process");
 // Require the necessary discord.js classes
-const { Client, Routes, Events, GatewayIntentBits, ActivityType, setPrestnce, SlashCommandBuilder } = require('discord.js');
+const { Client, Routes, Events, GatewayIntentBits, ActivityType, setPrestnce, SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { createInterface } = require('node:readline');
 const fetch = require('node-fetch');
 const { token } = require('./config.json');
@@ -85,14 +86,14 @@ const total = new SlashCommandBuilder()
 
 const bancheck = new SlashCommandBuilder()
     .setName('bancheck')
-    .setDescription('🔍 Проверка наличия домена или IP адреса в реестре Роскомнадзора')
+    .setDescription('🔍 Проверка наличия домена или IPv4 адреса в реестре Роскомнадзора')
 // Interaction work with 0 - Guild Install, 1 - User Install
     .setIntegrationTypes(0, 1)
 // Interaction can be used in 0 - Guild Channels, 1 - DM with bot, 2 - Group or Private user DM's
     .setContexts(0, 1, 2)
     .addStringOption(option =>
         option.setName('type')
-            .setDescription('🔍 Блокировку чего нужно проверить? Домена/Сайта или IP адреса?')
+            .setDescription('🔍 Блокировку чего нужно проверить? Домена/Сайта или IPv4 адреса?')
             .setRequired(true)
             .addChoices(
                 { name: 'Домен (Например: instagram.com)', value: 'domain' },
@@ -100,7 +101,7 @@ const bancheck = new SlashCommandBuilder()
             ))
     .addStringOption(option =>
         option.setName('string')
-            .setDescription('🔍 Имя домена маленькими буквами без https:// или корректный ip адрес (Например: 1.1.1.1)')
+            .setDescription('🔍 Имя домена маленькими буквами без https:// или корректный IPv4 адрес (Например: 1.1.1.1)')
             .setMinLength(5)
             .setMaxLength(255)
             .setRequired(true))
@@ -116,103 +117,93 @@ const commands = [ping, bancheck, about, invite, total]; // Add your commands wi
 
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
-client.on('interactionCreate', (interaction) => {
-    if (interaction.options.getBoolean('публично') === undefined || interaction.options.getBoolean('публично') === null || interaction.options.getBoolean('публично') === false) {
-        var isephemeral = true
-        var publicreplylog = ''
-    } else { 
+//functions
+//async delay
+function delay(ms) { //usage: await delay(10000)
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+//ephemeral message?
+async function checkephemeral(interaction) {
+    const isPublic = interaction.options.getBoolean('публично') ?? false;
+    if (isPublic) {
         incrementStat(`use.publicreply`);
-        var publicreplylog = 'public'
-        var isephemeral = false
+        return { publicreplylog: 'public', isephemeral: false };
     }
+    return { publicreplylog: '', isephemeral: true };
+}
+//interaction functions
+//common reply
+async function interactionreply(interaction, replycontent, isephemeral) {
+    //djs v14.15+ now using flags instead of 'ephemeral: true'
+    const replyflag = [];
+    if (isephemeral) replyflag.push(MessageFlags.Ephemeral);
+    //if (hideembeds) replyflag.push(MessageFlags.SuppressEmbeds);
+    await interaction.reply({
+        content: replycontent,
+        flags: replyflag,
+    });
+}
+//common reply (deferred)
+async function interactioneditreply(interaction, replycontent, isephemeral) {
+    await interaction.editReply({
+        content: replycontent,
+    });
+}
+//bancheck
+async function fbancheck(interaction, isephemeral, publicreplylog) {
+    const search = interaction.options.getString('string');
+    if (forbiddenChars.test(search)) {
+        const replycontent = `:warning: **В запросе запрещено использовать специальные символы!**`
+        return await interactioneditreply(interaction, replycontent, true);
+    }
+    let mode;
+    const reqid = Math.random();
+    if (interaction.options.getString('type') === 'domain') {
+        mode = ''
+        incrementStat('domainchecked');
+        console.log(`Bancheck: '${search}' ${publicreplylog}`)
+    } else {
+        mode = 'ip'
+        incrementStat('ipchecked');
+        console.log(`Bancheck: ip:'${search}' ${publicreplylog}`)
+    }
+    await new Promise(r => exec(`/bin/bash ./domfind.sh '${search}' '${reqid}' '${mode}'`, (r)));
+    //Read callback and reply
+    let domaindata;
+    const filepath = path.join(__dirname, `/temp/${reqid}`);
+    try {
+        domaindata = await fspromises.readFile(filepath, 'utf8');
+    } catch (error) {
+        console.error('Failed to read file:', error);
+    }
+    const replycontent = domaindata || `:warning: *Ошибка сервера. Обратитесь к администратору бота. Код: undefined_reply*`
+    interactioneditreply(interaction, replycontent, isephemeral);
+}
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    //check if reply be ephemeral
+    let { publicreplylog, isephemeral } = await checkephemeral(interaction);
     if (interaction.commandName === 'ping') {
+        const replycontent = `:ping_pong: *Понг!* Задержка ${Date.now() - interaction.createdTimestamp} миллисекунд! Задержка API ${Math.round(client.ws.ping)} миллисекунд.`
+        await interactionreply(interaction, replycontent, true);
         incrementStat('pingcmd');
-        interaction.reply({
-            content: `:ping_pong: *Понг!* Задержка ${Date.now() - interaction.createdTimestamp} миллисекунд! Задержка API ${Math.round(client.ws.ping)} миллисекунд.`,
-            ephemeral: true,
-        });
     } else if (interaction.commandName === 'about') {
         incrementStat('aboutcmd');
-        interaction.reply({
-            content: `:blue_heart: Помогаю с поисками в реестре блокировок! Начните поиск с помощью команды **/bancheck**. Пригласите на свой сервер с помощью **/invite**. Статистику по блокировкам сегодня можно посмотреть с помощью **/total**.\n:speech_left: Бот Запретян работает на базе https://github.com/SHULKERPLAY/Zapretyan (Оригинальная: \`Zapretyan#2802\`).\n:dizzy: *Версия ядра: ${corever}*\n:grey_question: Есть вопросы? [Посмотрите FAQ на Github](https://github.com/SHULKERPLAY/Zapretyan/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE-%D0%B7%D0%B0%D0%B4%D0%B0%D0%B2%D0%B0%D0%B5%D0%BC%D1%8B%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B) или [на нашем сайте!](https://lunarcreators.ru/zapretyan/app/) \n:gift_heart: [Сервер поддержки](https://discord.gg/e2HcXrQ) - <@459657842895486977> \n\n[Условия использования](https://lunarcreators.ru/zapretyan/app/tos/) и [Политика Конфиденциальности](https://lunarcreators.ru/zapretyan/app/privacy/)`,
-            ephemeral: true,
-        });
+        const replycontent = `:blue_heart: Помогаю с поисками в реестре блокировок! Начните поиск с помощью команды **/bancheck**. Пригласите на свой сервер с помощью **/invite**. Статистику по блокировкам сегодня можно посмотреть с помощью **/total**.\n:speech_left: Бот Запретян работает на базе https://github.com/SHULKERPLAY/Zapretyan (Оригинальная: \`Zapretyan#2802\`).\n:dizzy: *Версия ядра: ${corever}*\n:grey_question: Есть вопросы? [Посмотрите FAQ на Github](https://github.com/SHULKERPLAY/Zapretyan/wiki/%D0%A7%D0%B0%D1%81%D1%82%D0%BE-%D0%B7%D0%B0%D0%B4%D0%B0%D0%B2%D0%B0%D0%B5%D0%BC%D1%8B%D0%B5-%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81%D1%8B) или [на нашем сайте!](https://lunarcreators.ru/zapretyan/app/) \n:gift_heart: [Сервер поддержки](https://discord.gg/e2HcXrQ) - <@459657842895486977> \n\n-# [Условия использования](https://lunarcreators.ru/zapretyan/app/tos/) и [Политика Конфиденциальности](https://lunarcreators.ru/zapretyan/app/privacy/)`
+        await interactionreply(interaction, replycontent, true);
     } else if (interaction.commandName === 'invite') {
         incrementStat('invitecmd');
-        interaction.reply({
-            content: `:gift_heart: [Нажмите для добавления бота на сервер](https://discord.com/oauth2/authorize?client_id=907372459144147035&permissions=277025410048&integration_type=0&scope=bot) или [Добавьте через магазин приложений на сервер или как личное приложение](https://discord.com/discovery/applications/907372459144147035)! \n*Установка в свои приложения даёт доступ к функциям поиска запретян в любом чате сервера и ЛС.* \n\n:bangbang: *Это **НЕ рассылки**! На вашем сервере будут доступны слеш-команды для поиска по реестру РКН. Для реализации ежедневных рассылок для вашего сервера свяжитесь с разработчиком.*`,
-            ephemeral: true,
-        });
+        const replycontent = `:gift_heart: [Нажмите для добавления бота на сервер](https://discord.com/oauth2/authorize?client_id=907372459144147035&permissions=277025410048&integration_type=0&scope=bot) или [Добавьте через магазин приложений на сервер или как личное приложение](https://discord.com/discovery/applications/907372459144147035)! \n*Установка в свои приложения даёт доступ к функциям поиска запретян в любом чате сервера и ЛС.* \n\n:bangbang: *Это **НЕ рассылки**! На вашем сервере будут доступны слеш-команды для поиска по реестру РКН. Для реализации ежедневных рассылок для вашего сервера свяжитесь с разработчиком.*`
+        await interactionreply(interaction, replycontent, true);
     } else if (interaction.commandName === 'total') {
         incrementStat('totalcmd');
-        interaction.reply({
-            content: `**__ДОМЕНЫ__**\n:fire: Сегодня заблокировано: __${banstats.todayban}__\n:large_blue_diamond: Сегодня разблокировано: __${banstats.todayunban}__\n:no_entry_sign: **Всего заблокировано: ${banstats.totalban}**\n\n**__IP АДРЕСА__**\n:orange_circle: Сегодня заблокировано: __${banstats.todayipban}__\n:green_circle: Сегодня разблокировано: __${banstats.todayipunban}__\n:x: **Всего заблокировано: ${banstats.totalipban}**`,
-            ephemeral: isephemeral,
-        });
+        const replycontent = `**__ДОМЕНЫ__**\n:fire: Сегодня заблокировано: __${banstats.todayban}__\n:large_blue_diamond: Сегодня разблокировано: __${banstats.todayunban}__\n:no_entry_sign: **Всего заблокировано: ${banstats.totalban}**\n\n**__IP АДРЕСА__**\n:orange_circle: Сегодня заблокировано: __${banstats.todayipban}__\n:green_circle: Сегодня разблокировано: __${banstats.todayipunban}__\n:x: **Всего заблокировано: ${banstats.totalipban}**`
+        await interactionreply(interaction, replycontent, isephemeral);
     } else if(interaction.commandName === 'bancheck') {
+        await interaction.deferReply({ flags: isephemeral ? [MessageFlags.Ephemeral] : [] });
+        await fbancheck(interaction, isephemeral, publicreplylog);
         incrementStat('getbancheck');
-        var type = interaction.options.getString('type');
-        if (type === 'domain') {
-            var domain = interaction.options.getString('string');
-            var domainid = Math.random();
-            console.log(`Bancheck: '${domain}' ${publicreplylog}`)
-            //Send request to shellscript
-            async function domfind() {
-                await new Promise(r => exec(`/bin/bash ./domfind.sh '${domain}' '${domainid}'`, (r))
-                );
-                //Read callback and reply
-                fs.readFile(path.join(__dirname,"/temp/"+domainid+""), 'utf8', (err, domaindata) => {
-                    if (domaindata === undefined || domaindata === null) {
-                        interaction.reply({
-                            content: `:warning: *Ошибка сервера. Обратитесь к администратору бота. Код: undefined_reply*`,
-                            ephemeral: true,
-                        });
-                    } else {
-                        interaction.reply({
-                            content: domaindata,
-                            ephemeral: isephemeral,
-                        });
-                        incrementStat('domainchecked');
-                    }
-                });
-            }
-            if (forbiddenChars.test(domain)) {
-                interaction.reply({
-                    content: `:warning: **В запросе запрещено использовать специальные символы!**`,
-                    ephemeral: true,
-                });
-            } else domfind();
-        } else if (type === 'ip') {
-            var ip = interaction.options.getString('string');
-            var ipid = Math.random();
-            console.log(`Bancheck: ip:'${ip}' ${publicreplylog}`)
-            //Send request to shellscript
-            async function ipfind() {
-                    await new Promise(r => exec(`/bin/bash ./domfind.sh '${ip}' '${ipid}' ip`, (r))
-                );
-                //Read callback and reply
-                fs.readFile(path.join(__dirname,"/temp/"+ipid+""), 'utf8', (err, domaindata) => {
-                    if (domaindata === undefined || domaindata === null) {
-                        interaction.reply({
-                            content: `:warning: *Ошибка сервера. Обратитесь к администратору бота. Код: undefined_reply*`,
-                            ephemeral: true,
-                        });
-                    } else {
-                        interaction.reply({
-                            content: domaindata,
-                            ephemeral: isephemeral,
-                        });
-                        incrementStat('ipchecked');
-                    }         
-                });
-            }
-            if (forbiddenChars.test(ip)) {
-                interaction.reply({
-                    content: `:warning: **В запросе запрещено использовать специальные символы!**`,
-                    ephemeral: true,
-                });
-            } else ipfind();
-        }
     }
 });
 
